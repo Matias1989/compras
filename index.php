8e53f9a53f19bec0d06c5a4a797ef09b622702a6
Mati Acosta <acostamatias.1989@gmail.com>
2018-12-03 02:34:34 -0300

    Menu principal

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<HTML lang="es" xml:lang = "es">
+	<HEAD>
+		<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+		<title>Menu Principal.</title>
+		<script src="js/funciones.js"></script>
+		<style>
+            
+            img{
+                image-resolution: from-image;
+                display:block;
+                margin:auto;
+                width: 600px;
+                height: 450px;
+            }
+            
+        </style>
+	</HEAD>
+	<body bgcolor="#E0E6F8">
+		<div align="center">
+			<h1 align="center">Libreria "V & D" - Sistemas de Compras</h1>
+			<a href="listado_proveedor.php"><button><i>Proveedores</i></button></a>
+			<a href="orden_compra.php"><button><i>Compras</i></button></a>
+		    <a href="listado_producto.php"><button><i>Productos</i></button></a>
+		</div>
+		<br>
+		<div>
+            <img src="img/fondo.png">
+		</div>
+		<footer>
+			<p align="center">Material recopilado y organizado por <b>Matías E. Acosta.</b></p>
+		</footer>
+	</body>
+</HTML>
